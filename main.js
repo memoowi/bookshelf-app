538d334b53a64c2c8a3440c933043edb60a75d1c
Misbachul Munir Al Haddad Alwi <megatenlike@gmail.com>
2024-03-08 03:06:12 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,168 @@
+const bookshelf = {
+  books: [],
+  findBookIndex: function (id) {
+    for (const index in this.books) {
+      if (this.books[index].id == id) {
+        return index;
+      }
+    }
+
+    return -1;
+  },
+  addBook: function (book) {
+    this.books.push(book);
+    this.saveToLocalStorage();
+  },
+  moveBook: function (id) {
+    const index = this.findBookIndex(id);
+
+    if (index !== -1) {
+      this.books[index].isComplete = !this.books[index].isComplete;
+      this.saveToLocalStorage();
+    } else {
+      throw new Error("Book not found");
+    }
+  },
+  deleteBook: function (id) {
+    const index = this.findBookIndex(id);
+
+    if (index !== -1) {
+      this.books.splice(index, 1);
+      this.saveToLocalStorage();
+    } else {
+      throw new Error("Book not found");
+    }
+  },
+  saveToLocalStorage: function () {
+    localStorage.setItem("bookshelf", JSON.stringify(this.books));
+  },
+  loadFromLocalStorage: function () {
+    const books = localStorage.getItem("bookshelf");
+    if (books) {
+      this.books = JSON.parse(books);
+    }
+  },
+};
+
+async function fetchSvgIcon(url) {
+  const response = await fetch(url);
+  const svgText = await response.text();
+  return svgText;
+}
+
+async function renderBooks(books) {
+  const incompleteBookshelfList = document.getElementById(
+    "incompleteBookshelfList"
+  );
+  const completeBookshelfList = document.getElementById(
+    "completeBookshelfList"
+  );
+
+  incompleteBookshelfList.innerHTML = "";
+  completeBookshelfList.innerHTML = "";
+
+  const trashIconText = await fetchSvgIcon("assets/icons8-trash.svg");
+  const doneIconText = await fetchSvgIcon("assets/icons8-done.svg");
+  const cancelIconText = await fetchSvgIcon("assets/icons8-cancel.svg");
+
+  (books || bookshelf.books).forEach((book) => {
+    const bookElement = document.createElement("article");
+    bookElement.classList.add("book_item");
+    bookElement.innerHTML = `
+          <h3>${book.title}</h3>
+          <p>Author: ${book.author}</p>
+          <p>Year: ${book.year}</p>
+          <div class="action">
+            <button class="edit" onclick="moveBook('${book.id}')" title="Move"></button>
+            <button class="delete" onclick="deleteBook('${book.id}')" title="Delete">${trashIconText}</button>
+          </div>
+        `;
+
+    if (book.isComplete) {
+      bookElement.querySelector(
+        ".edit"
+      ).innerHTML = cancelIconText;
+      completeBookshelfList.appendChild(bookElement);
+    } else {
+      bookElement.querySelector(
+        ".edit"
+      ).innerHTML = doneIconText;
+      incompleteBookshelfList.appendChild(bookElement);
+    }
+  });
+}
+
+function addBook(event) {
+  event.preventDefault();
+
+  const id = +new Date();
+  const title = document.getElementById("inputBookTitle").value;
+  const author = document.getElementById("inputBookAuthor").value;
+  const year = document.getElementById("inputBookYear").value;
+  const isComplete = document.getElementById("inputBookIsComplete").checked;
+
+  const book = { id, title, author, year, isComplete };
+  bookshelf.addBook(book);
+
+  document.getElementById("inputBook").reset();
+  document.getElementById("searchBook").reset();
+  renderBooks();
+}
+
+function moveBook(id) {
+  bookshelf.moveBook(id);
+
+  const isSearched = document.getElementById("searchBookTitle").value;
+
+  if (isSearched !== "") {
+    searchBook(event);
+  } else {
+    renderBooks();
+  }
+}
+
+function deleteBook(id) {
+  bookshelf.deleteBook(id);
+
+  const isSearched = document.getElementById("searchBookTitle").value;
+
+  if (isSearched !== "") {
+    searchBook(event);
+  } else {
+    renderBooks();
+  }
+}
+
+function searchBook(event) {
+  event.preventDefault();
+
+  const keyword = document.getElementById("searchBookTitle").value;
+
+  const filteredBooks = bookshelf.books.filter((book) => {
+    return (
+      book.title.toLowerCase().includes(keyword.toLowerCase()) ||
+      book.author.toLowerCase().includes(keyword.toLowerCase())
+    );
+  });
+
+  renderBooks(filteredBooks);
+}
+
+bookshelf.loadFromLocalStorage();
+document
+  .getElementById("inputBookIsComplete")
+  .addEventListener("change", function () {
+    if (this.checked) {
+      document
+        .getElementById("bookSubmit")
+        .getElementsByTagName("span")[0].textContent = "Finished";
+    } else {
+      document
+        .getElementById("bookSubmit")
+        .getElementsByTagName("span")[0].textContent = "Unfinished";
+    }
+  });
+
+document.getElementById("inputBook").addEventListener("submit", addBook);
+document.getElementById("searchBook").addEventListener("submit", searchBook);
+renderBooks();
